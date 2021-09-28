9f6f0357e353665a4462959e96e39c540a2c5e57
Jakob Tómas Bullerjahn <bullerjahn@gmail.com>
2021-09-28 11:59:35 +0200

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.18

---
+++ src/ForceSpectroscopyMLE.jl
@@ -0,0 +1,5 @@
+module ForceSpectroscopyMLE
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using ForceSpectroscopyMLE
+using Test
+
+@testset "ForceSpectroscopyMLE.jl" begin
+    # Write your tests here.
+end
